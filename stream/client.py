08aa77438ab9041c24844e45928c51fa2df30881
tijs.klaren <tijs.q.klaren@gmail.com>
2021-09-29 14:37:51 +0200

    Simple client and server image sharing

---
+++ stream/client.py
@@ -0,0 +1,20 @@
+# import the necessary packages
+from imutils.video import VideoStream
+import imagezmq
+import argparse
+import socket
+import time
+
+server_ip = "192.168.0.11"
+
+sender = imagezmq.ImageSender(connect_to="tcp://{}:5555".format(server_ip))
+
+
+rpiName = socket.gethostname()
+vs = VideoStream(usePiCamera=False).start()
+
+time.sleep(2.0)
+ 
+while True:
+    frame = vs.read()
+    sender.send_image(rpiName, frame)
