3b55a51d00a6d3404cdc5cd9dd08eb169e9477c6
Ongun1905 <53625744+Ongun1905@users.noreply.github.com>
2021-09-30 13:43:19 +0200

    server code complete

--- stream/server.py
+++ stream/server.py
@@ -9,6 +9,37 @@ classesInput = os.path.abspath("./yolov3.txt")
 
 imageHub = imagezmq.ImageHub()
 
+def get_output_layers(net):
+    layer_names = net.getLayerNames()
+
+    output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+    return output_layers
+
+
+
+def draw_prediction(img, class_id, confidence, x, y, x_plus_w, y_plus_h):
+    label = str(classes[class_id])
+
+    color = COLORS[class_id]
+
+    center_x = round((x_plus_w-x)/2+x)
+    center_y = round((y_plus_h-y)/2+y)
+
+    # Compute percentage from centerpoint
+    d_center_x = round(((2*center_x)/Width - 1)*100)
+    d_center_y = round((1 - (2*center_y)/Height)*100)
+
+    # Rectangle around complete object
+    cv2.rectangle(img, (x, y), (x_plus_w, y_plus_h), color, 2)
+
+    # Circle in center of object
+    cv2.circle(img, (center_x,center_y), 10, (0, 0, 255), 0)
+
+    # Put text on top
+    cv2.putText(img, (label + ' ' + str(d_center_x) + '% ' + str(d_center_y) + '%'), (x - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+
 
 #read in the class definitions
 classes = None
@@ -21,15 +52,71 @@ COLORS = np.random.uniform(0, 255, size=(len(classes), 3))
 #setup the network
 net = cv2.dnn.readNet(weightsInput, configInput)
 
+# Some stylings
+font = cv2.FONT_HERSHEY_SIMPLEX
+bottomLeftCornerOfText = (10, 200)
+fontScale = 1
+fontColor = (255, 255, 255)
+lineType = 2
+
+
 
 
 while True:
     (rpiName, frame) = imageHub.recv_image()
+    Width = frame.shape[1]
+    Height = frame.shape[0]
     imageHub.send_reply(b'OK')
+
+    blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), (0, 0, 0), True, crop = False)
+
+    net.setInput(blob)
+
+    outs = net.forward(get_output_layers(net))
+
+    class_ids = []
+    confidences = []
+    boxes = []
+    conf_threshold = 0.5
+    nms_threshold = 0.4
+
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.5:
+                center_x = int(detection[0] * Width)
+                center_y = int(detection[1] * Height)
+                w = int(detection[2] * Width)
+                h = int(detection[3] * Height)
+                x = center_x - w / 2
+                y = center_y - h / 2
+                class_ids.append(class_id)
+                confidences.append(float(confidence))
+                boxes.append([x, y, w, h])
+
+    indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
+
+    for i in indices:
+        i = i[0]
+        box = boxes[i]
+        x = box[0]
+        y = box[1]
+        w = box[2]
+        h = box[3]
+        draw_prediction(frame, class_ids[i], confidences[i], round(x), round(y), round(x + w), round(y + h))
+
+    class_list = [i[0] for i in indices]
+
+
     cv2.imshow('Input', frame)
 
-    c = cv2.waitKey(1)
-    if c == 27:
+    
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        imageHub.close()
+        cv2.destroyAllWindows()
+        print('server closed')
         break
 
-cv2.destroyAllWindows()
