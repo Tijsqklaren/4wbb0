0b3cd2b07424adfd95c4b100e59e54baf8631393
tijs.klaren <tijs.q.klaren@gmail.com>
2021-09-30 14:10:48 +0200

    Client side

--- SafeSeeClient.py
+++ SafeSeeClient.py
@@ -3,23 +3,60 @@ from imutils.video import VideoStream
 import imagezmq
 import socket
 import speech_recognition as sr
+import json
+from threading import Thread
+import time
 
 def activateSpeech(){
-    
+    objectLabel = "bottle"
+
+    sendQuery = {'type': 'objectLabel', 'value': objectLabel}
+
+    mySocket.sendto(json.dumps(sendQuery),(SERVER_IP,PORT_NUMBER))
+
+    activateSearch()
 }
 
 def powerOFF(){
     subprocess.call(['shutdown', '-h', 'now'], shell=False)
 }
 
+# Search for the object
 def activateSearch(){
+    # initiate a while loop
+    beepGenerator.active = False
+
+    t = Thread(target=beepGenerator)
+    t.start()
     while True:
+        # Get the frame and send it
         frame = vs.read()
         sender.send_image(rpiName, frame)
+
+        # Receive data back from the server
+        (data,addr) = mySocket.recvfrom(SIZE)
+        data = data.decode('utf8', 'strict')
+        if(len(data)):
+            dataDict = json.loads(data)
+            if(len(dataDict)):
+                if(dataDict['type'] == "centerDiff"):
+                    percentageValue = dataDict['value']
+                    beepGenerator.active = True
+                    frequency = percentageValue
+
+        # If the object is found, quit the loop
         if GPIO.input(pins['finishPIN']) == GPIO.HIGH:
             break
         
 }
+
+# Beep generator
+frequency = 1
+def beepGenerator():
+    while not beepGenerator.active:
+        print(datetime.now(), 'start')
+        time.sleep(1/frequency)
+        print(datetime.now(), 'end')
 
 # Init pins
 pins = {'speechPIN': 0, 'powerPIN': 1, 'finishPIN': 2}
