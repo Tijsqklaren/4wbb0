24a8d302bec79bb3dbf3b8c70529a48f33954e41
Ongun1905 <53625744+Ongun1905@users.noreply.github.com>
2021-09-30 11:51:57 +0200

    Implemented speech recog and workin on server

--- speechrecognition_test.py
+++ speechrecognition_test.py
@@ -4,16 +4,27 @@
 
 import speech_recognition as sr
 
-# obtain audio from the microphone
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Say something!")
-    audio = r.listen(source)
 
-# recognize speech using Sphinx
-try:
-    print("Sphinx thinks you said " + r.recognize_sphinx(audio))
-except sr.UnknownValueError:
-    print("Sphinx could not understand audio")
-except sr.RequestError as e:
-    print("Sphinx error; {0}".format(e))
+def main():
+
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source)
+
+        audio = r.listen(source)
+
+        try:
+
+            print(r.recognize_google(audio))
+
+        except Exception as e:
+            print("Error :  " + str(e))
+
+
+        with open("recorded.wav", "wb") as f:
+            f.write(audio.get_wav_data())
+
+
+if __name__ == "__main__":
+    main()
