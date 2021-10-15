2980691f404d69e25c2a53a2138c13f4f3445eab
tijs.klaren <tijs.q.klaren@gmail.com>
2021-10-15 14:39:00 +0200

    Did some commenting and printing, and added a handshake between the server and client

--- stream/Client-Side/SafeSeeClient.py
+++ stream/Client-Side/SafeSeeClient.py
@@ -10,6 +10,13 @@ import time
 import sys
 import subprocess
 
+# Set variables
+imagePort = 5555
+dataPort = 8000
+SIZE = 1024
+serverConfigured = False
+percentageValue = False
+
 def activateSpeech(self):
     objectLabel = "bottle"
 
@@ -20,15 +27,31 @@ def activateSpeech(self):
     activateSearch()
 
 def powerOFF(self):
-    print('power off')
+    print('powering device off')
+
+    # Tell the server to also terminate the connection
+    sendQuery = {'type': 'clientPowerOff', 'value': True}
+    mySocket.sendto(str.encode(json.dumps(sendQuery)),(server_ip,dataPort))
+
+    # Actually shut down
     subprocess.call(['sudo', 'shutdown', '-h', 'now'], shell=False)
 
 # Search for the object
 def activateSearch():
+<<<<<<< master
     # initiate a while loop
 
     buzzerThread = Thread(target=beepGenerator)
     buzzerThread.start()
+=======
+    beepGenerator.active = False
+
+    beepGeneratorThread = Thread(target=beepGenerator)
+    beepGeneratorThread.start()
+
+    getDiffThread = Thread(target=getDiff)
+    getDiffThread.start()
+>>>>>>> local
     vs = VideoStream(usePiCamera=False).start()
 
     while True:
@@ -37,7 +60,7 @@ def activateSearch():
         sender.send_image(rpiName, frame)
 
         # If the object is found, quit the loop
-        if GPIO.input(pins['finishPIN']) == GPIO.LOW:
+        if GPIO.input(buttonPins['finishPIN']) == GPIO.LOW:
             print('Quit button pressed')
             sendQuery = {'type': 'searchFinished', 'value': True}
             mySocket.sendto(str.encode(json.dumps(sendQuery)),(server_ip,dataPort))
@@ -47,12 +70,26 @@ def activateSearch():
             break
 
 # Beep generator
-frequency = 1
+frequencyRange = [1, 100]
 def beepGenerator():
+<<<<<<< master
     while beepGenerator.active:
         print('start')
         time.sleep(1/frequency)
         print('end')                
+=======
+    global percentageValue
+    while beepGenerator.active:
+        if percentageValue > 0:
+            # Lower percentages -> frequency closer to upper part of range, higher percentage -> frequency closer to lower part of range
+            frequencyRange = frequencyRange[0] + (frequencyRange[1]-frequencyRange[0]) * (1-abs(percentageValue)/100)
+            # Start buzzer
+            GPIO.output(buzzerPin, GPIO.HIGH)
+            time.sleep(1/(2*frequency))
+            # Stop buzzer
+            GPIO.output(buzzerPin, GPIO.LOW)
+            time.sleep(1/(2*frequency))
+>>>>>>> local
 
 # Listen to input from server
 def socketThreadFunc():
@@ -63,17 +100,50 @@ def socketThreadFunc():
         if(len(data)):
             dataDict = json.loads(data)
             if(len(dataDict)):
+<<<<<<< master
                 if(dataDict['type'] == "localizationStart"):
                     print('Start pinging for device')
                 elif(dataDict['type'] == "localizationEnd"):
                     print('Stop pinging for device')
                 elif(dataDict['type'] == "centerDiff"):
+=======
+                if(dataDict['type'] == "centerDiff"):
+                    print('object found')
+                    global percentageValue
+>>>>>>> local
                     percentageValue = dataDict['value']
                     beepGenerator.active = True
-                    print(percentageValue)
+
+def initSockets():
+    # Set op image socket
+    hostName = gethostbyname('0.0.0.0')
+
+    mySocket = socket(AF_INET, SOCK_DGRAM)
+    mySocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    mySocket.bind((hostName, dataPort))
+
+    print("Waiting for server's connection")
+    (data,addr) = mySocket.recvfrom(SIZE)
+    data = data.decode('utf8', 'strict')
+    if(len(data)):
+        dataDict = json.loads(data)
+        if(len(dataDict)):
+            if(dataDict['type'] == "serverHandshake"):
+                server_ip = dataDict['server_ip']
+                
+                sendQuery = {'type': 'clientHandshake', 'value': True}
+                mySocket.sendto(str.encode(json.dumps(sendQuery)),(server_ip,dataPort))
+
+                print("Server connected on " + server_ip)
+                
+                sender = imagezmq.ImageSender(connect_to="tcp://{ip}:{port}".format(ip = server_ip, port = dataPort))
+                rpiName = gethostbyname('0.0.0.0')
+                serverConfigured = True
 
 # Init pins
+# Set GPIO mode to board (thus using the printed numbering on the pi)
 GPIO.setmode(GPIO.BOARD)
+<<<<<<< master
 pins = {'speechPIN': 8, 'powerPIN': 5, 'finishPIN': 10}
 for pin in pins:
     GPIO.setup(pins[pin], GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -118,3 +188,27 @@ while True:
         activateSpeech(True)
     if GPIO.input(pins['powerPIN']) == GPIO.LOW:
         powerOFF(True)
+=======
+# Name the pins used for buttons
+buttonPins = {'speechPIN': 8, 'powerPIN': 5, 'finishPIN': 10}
+# Loop through the buttons to setup
+for pin in buttonPins:
+    GPIO.setup(buttonPins[pin], GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+# Setup the buzzer pin
+buzzerPin = 12
+GPIO.setup(buzzerPin, GPIO.OUT)
+
+# Run the function to initialize the sockets
+initSockets()
+    
+# Set event listeners
+while True:
+    if serverConfigured:
+        if GPIO.input(buttonPins['speechPIN']) == GPIO.LOW:
+            activateSpeech(True)
+        if GPIO.input(buttonPins['powerPIN']) == GPIO.LOW:
+            powerOFF(True)
+    else:
+        initSockets()
+>>>>>>> local
--- stream/Server-Side/server.py
+++ stream/Server-Side/server.py
@@ -2,7 +2,11 @@ import imagezmq
 import cv2
 import os
 import numpy as np
+<<<<<<< master
 from socket import socket, gethostbyname, AF_INET, SOCK_DGRAM, gethostname
+=======
+from socket import socket, gethostbyname, gethostname, AF_INET, SOCK_DGRAM
+>>>>>>> local
 import json
 from threading import Thread
 
@@ -11,19 +15,12 @@ configInput = os.path.abspath("./yolov3.cfg")
 weightsInput = os.path.abspath("./yolov3.weights")
 classesInput = os.path.abspath("./yolov3.txt")
 
-# Initialize sockets
-imageHub = imagezmq.ImageHub()
-hostName = gethostbyname('0.0.0.0')
-client_ip = "192.168.0.10"
-dataPort = 5000
-SIZE = 1024
-
-mySocket = socket(AF_INET, SOCK_DGRAM)
-mySocket.bind((hostName, dataPort))
-
 # variables
 lostObjectLabel = ""
 terminateSearch = False
+
+dataPort = 5000
+SIZE = 1024
 
 
 def get_output_layers(net):
@@ -33,52 +30,78 @@ def get_output_layers(net):
 
     return output_layers
 
-
-
 def draw_prediction(img, class_id, confidence, x, y, x_plus_w, y_plus_h, d_center_x, d_center_y):
     label = str(classes[class_id])
-
-    color = COLORS[class_id]
 
     center_x = round((x_plus_w-x)/2+x)
     center_y = round((y_plus_h-y)/2+y)
 
     # Rectangle around complete object
-    cv2.rectangle(img, (x, y), (x_plus_w, y_plus_h), color, 2)
+    cv2.rectangle(img, (x, y), (x_plus_w, y_plus_h), objectColor, 2)
 
     # Circle in center of object
     cv2.circle(img, (center_x,center_y), 10, (0, 0, 255), 0)
 
     # Put text on top
-    cv2.putText(img, (label + ' ' + str(d_center_x) + '% ' + str(d_center_y) + '%'), (x - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+    cv2.putText(img, (label + ' ' + str(d_center_x) + '% ' + str(d_center_y) + '%'), (x - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, objectColor, 2)
 
-def listenForTermination():
-    global terminateSearch
+def listenForInput():
     (data,addr) = mySocket.recvfrom(SIZE)
     receivedData = data.decode('utf8', 'strict')
     if len(receivedData):
         dataDict = json.loads(receivedData)
-        if(dataDict['type'] == "searchFinished"):
+        if dataDict['type'] == "searchFinished":
+            global terminateSearch
             terminateSearch = True
+        elif dataDict['type'] == "clientPowerOff":
+            print('Client is powering off, so reset sockets')
+            initSockets()
+
+
+def initSockets():
+    # Initialize sockets
+    imageHub = imagezmq.ImageHub()
+    hostName = gethostbyname('0.0.0.0')
+
+    client_ip = "192.168.0.10"
+    server_ip = gethostbyname(gethostname())
+
+    mySocket = socket(AF_INET, SOCK_DGRAM)
+    mySocket.bind((hostName, dataPort))
+
+    print('Sockets started, waiting for confirmation from client')
+
+    # Send the server's IP-addres
+    mySocket.sendto(str.encode(json.dumps({"type": "serverHandshake", "server_ip": server_ip})),(client_ip,dataPort))
+    
+    # Wait for confirmation from client
+    (data,addr) = mySocket.recvfrom(SIZE)
+    receivedData = data.decode('utf8', 'strict')
+    if len(receivedData):
+        dataDict = json.loads(receivedData)
+        if(dataDict['type'] == "clientHandshake"):
+            serverConfigured = True
+            print('Client confirmed connection')
+            listenForInputThread = Thread(target=listenForInput)
+            listenForInputThread.start()
 
 #read in the class definitions
 classes = None
 with open(classesInput, 'r') as f:
     classes = [line.strip() for line in f.readlines()]
 
-COLORS = np.random.uniform(0, 255, size=(len(classes), 3))
 
-
-#setup the network
+#setup the deep neural network
 net = cv2.dnn.readNet(weightsInput, configInput)
 
-# Some stylings
+# Some styling
 font = cv2.FONT_HERSHEY_SIMPLEX
 bottomLeftCornerOfText = (10, 200)
 fontScale = 1
-fontColor = (255, 255, 255)
+objectColor = (255, 255, 255)
 lineType = 2
 
+<<<<<<< master
 # Give the server's IP to the client
 mySocket.sendto(str.encode(json.dumps({"type": "serverData", "server_ip": socket.gethostbyname(socket.gethostname())})),(client_ip,dataPort))
 (data,addr) = mySocket.recvfrom(SIZE)
@@ -88,14 +111,19 @@ if len(receivedData):
     if dataDict['type'] == 'serverDataReceived':
         if dataDict['value']:
             print('Server data shared succesfully')
+=======
+# Run the function to initialize the sockets
+initSockets()
+>>>>>>> local
 
 while True:
     if terminateSearch:
-        print('terminating search executed')
+        print('Terminated search')
         lostObjectLabel == ""
         cv2.destroyAllWindows()
+        continue
 
-    if(lostObjectLabel == "" or terminateSearch):
+    if(lostObjectLabel == ""):
         (data,addr) = mySocket.recvfrom(SIZE)
         receivedData = data.decode('utf8', 'strict')
         if len(receivedData):
@@ -104,56 +132,52 @@ while True:
             if(dataDict['type'] == "objectLabel"):
                 lostObjectLabel = dataDict['value']
                 print("Searching for: " + lostObjectLabel)
-                listenForTerminationThread = Thread(target=listenForTermination)
-                listenForTerminationThread.start()
                 terminateSearch = False
-            # If in yolov3.txt, still have to mke
 
-    if(True):
-        (rpiName, frame) = imageHub.recv_image()
-        Width = frame.shape[1]
-        Height = frame.shape[0]
-        imageHub.send_reply(b'OK')
+    (rpiName, frame) = imageHub.recv_image()
+    Width = frame.shape[1]
+    Height = frame.shape[0]
 
-        blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), (0, 0, 0), True, crop = False)
+    blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), (0, 0, 0), True, crop = False)
 
-        net.setInput(blob)
+    net.setInput(blob)
 
-        outs = net.forward(get_output_layers(net))
+    outs = net.forward(get_output_layers(net))
 
-        class_ids = []
-        confidences = []
-        boxes = []
-        conf_threshold = 0.5
-        nms_threshold = 0.4
+    class_ids = []
+    confidences = []
+    boxes = []
+    conf_threshold = 0.5
+    nms_threshold = 0.4
 
-        for out in outs:
-            for detection in out:
-                scores = detection[5:]
-                class_id = np.argmax(scores)
-                confidence = scores[class_id]
-                if confidence > 0.5:
-                    center_x = int(detection[0] * Width)
-                    center_y = int(detection[1] * Height)
-                    w = int(detection[2] * Width)
-                    h = int(detection[3] * Height)
-                    x = center_x - w / 2
-                    y = center_y - h / 2
-                    class_ids.append(class_id)
-                    confidences.append(float(confidence))
-                    boxes.append([x, y, w, h])
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.5:
+                center_x = int(detection[0] * Width)
+                center_y = int(detection[1] * Height)
+                w = int(detection[2] * Width)
+                h = int(detection[3] * Height)
+                x = center_x - w / 2
+                y = center_y - h / 2
+                class_ids.append(class_id)
+                confidences.append(float(confidence))
+                boxes.append([x, y, w, h])
 
-                    # Compute percentage from centerpoint
-                    d_center_x = round(((2*center_x)/Width - 1)*100)
-                    d_center_y = round((1 - (2*center_y)/Height)*100)
+                # Compute percentage from centerpoint
+                d_center_x = round(((2*center_x)/Width - 1)*100)
+                d_center_y = round((1 - (2*center_y)/Height)*100)
 
-                    if str(classes[class_id]) == lostObjectLabel:
-                        mySocket.sendto(str.encode(json.dumps({"type": "centerDiff", "value": d_center_x})),(client_ip,dataPort))
+                if str(classes[class_id]) == lostObjectLabel:
+                    mySocket.sendto(str.encode(json.dumps({"type": "centerDiff", "value": d_center_x})),(client_ip,dataPort))
 
-        indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
+    indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
 
-        for i in indices:
-            i = i[0]
+    for i in indices:
+        i = i[0]
+        if str(classes[class_ids[i]]) == lostObjectLabel:
             box = boxes[i]
             x = box[0]
             y = box[1]
@@ -161,13 +185,12 @@ while True:
             h = box[3]
             draw_prediction(frame, class_ids[i], confidences[i], round(x), round(y), round(x + w), round(y + h), d_center_x, d_center_y)
 
-        class_list = [i[0] for i in indices]
+    class_list = [i[0] for i in indices]
 
-        cv2.imshow('Input', frame)
+    cv2.imshow('Input', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         imageHub.close()
         cv2.destroyAllWindows()
         print('server closed')
         break
-
